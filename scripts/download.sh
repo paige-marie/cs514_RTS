8681127534a32f2e11ed91853a2aa09f23018db8
Paige Hansen <hansen.paigemarie@gmail.com>
2024-01-08 19:35:06 -0700

    script to clone repos for sequential commits

---
+++ scripts/download.sh
@@ -0,0 +1,63 @@
+#!/bin/env bash
+
+HOME_DIR=".."
+
+REVISIONS_DIR="$HOME_DIR/original_source"
+EKSTAZI_DIR="$HOME_DIR/Ekstazi"
+STARTS_DIR="$HOME_DIR/STARTS"
+
+mkdir $REVISIONS_DIR
+mkdir $EKSTAZI_DIR
+mkdir $STARTS_DIR
+
+cat ../repositories.txt |
+while IFS=" " read -r pName pURL pRevSNum; do
+	if [[ -z "$pName" ]]; then
+		break
+	fi
+
+	echo "### Clone project " $pName
+
+	mkdir "$REVISIONS_DIR/$pName"
+	echo "***" git clone $pURL "$REVISIONS_DIR/$pName/$pName"
+	git clone $pURL "$REVISIONS_DIR/$pName/$pName"
+
+	if [[ $pName == openTripPlanner ]]; then
+		cwd=$(pwd)
+		cd $REVISIONS_DIR/$pName/$pName
+		git checkout master
+		cd $cwd
+		echo "CURRENT PATH: " $cwd
+	fi
+
+	(git --git-dir $REVISIONS_DIR/$pName/$pName/.git log --reverse --ancestry-path $pRevSNum...master | grep -F "commit " | cut -f2 -d' ') > $REVISIONS_DIR/$pName/revisions.txt 
+
+	if [ ! -s $REVISIONS_DIR/$pName/revisions.txt ]  || [ ! -f $REVISIONS_DIR/$pName/revisions.txt ]; then
+		echo "ERROR: Failed to create revisions.txt!"
+		exit 2
+	fi
+
+	idx=1
+	max_idx=50
+
+	cat $REVISIONS_DIR/$pName/revisions.txt |
+	while IFS= read -r hashID; do
+		if [[ -z "$hashID" ]]; then
+			echo "skip hashID because the value is $hashID"
+			continue
+		fi
+
+		git clone $pURL "$REVISIONS_DIR/$pName/rev_$idx"
+		cwd=$(pwd)
+		cd $REVISIONS_DIR/$pName/rev_$idx
+		git checkout -f $hashID
+		cd $cwd
+
+		if ((idx >= max_idx)); then
+			break
+		fi
+
+		idx=$((idx + 1))
+	done
+done
+
