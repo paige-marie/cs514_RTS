865dc0550693196aaa09ef5597afa6b1cc69d336
Paige Hansen <hansen.paigemarie@gmail.com>
2024-01-08 19:45:03 -0700

    removes all created files

---
+++ scripts/clean.sh
@@ -0,0 +1,24 @@
+#!/bin/env bash
+
+HOME_DIR=".."
+
+echo "### cleaning... "
+
+echo "#### Delete original sources"
+rm -rf "$HOME_DIR/original_source"
+
+echo "#### Delete Ekstazi Dir"
+rm -rf "$HOME_DIR/Ekstazi"
+
+echo "#### Delete STARTS Dir"
+rm -rf "$HOME_DIR/STARTS"
+
+echo "#### Delete HyRTS Dir"
+rm -rf "$HOME_DIR/HyRTS"
+
+echo "#### Delete Clover Dir"
+rm -rf "$HOME_DIR/Clover"
+
+rm -rf ~/.local/share/Trash/*
+
+mv $HOME_DIR/result/ $HOME_DIR/result_first/
